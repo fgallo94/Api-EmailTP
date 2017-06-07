599d872a0d2e28e185808d08e771d9a5dace2e71
gallito1994 <gallito199409@gmail.com>
2017-06-07 18:55:16 -0300

    asdasd

---
+++ src/main/java/com/utn/api/email/Config.java
@@ -0,0 +1,28 @@
+package com.utn.api.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class Config {
+
+    @Autowired
+    AuthFilter authFilter;
+
+    @Value("${date.format}")
+    String dateFormat;
+
+
+    @Bean
+    public FilterRegistrationBean myFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(authFilter);
+        registration.addUrlPatterns("/api/*");
+        return registration;
+    }
+
+}
